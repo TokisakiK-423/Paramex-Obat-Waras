0985ff5e1a06a337d20005712695438f71dc1132
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:34:03 +0700

    Update auth.service.ts

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,53 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import * as bcrypt from 'bcrypt';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private prisma: PrismaService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
-  async register(data: any) {
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
-    const user = await this.prisma.user.create({
-      data: {
-        email: data.email,
-        password: hashedPassword,
-        role: 'CUSTOMER',
-      },
-    });
-
-    return {
-      message: 'Register berhasil',
-      user,
-    };
-  }
-
-  async login(data: any) {
-    const user = await this.prisma.user.findUnique({
-      where: { email: data.email },
-    });
-
-    if (!user) throw new UnauthorizedException('User tidak ditemukan');
-
-    const isMatch = await bcrypt.compare(data.password, user.password);
-
-    if (!isMatch) throw new UnauthorizedException('Password salah');
-
-    const payload = {
-      sub: user.id,
-      email: user.email,
-      role: user.role,
-    };
+  async login(username: string, password: string) {
+    const admin = await this.prisma.admin.findUnique({ where: { username } });
+    if (!admin || admin.password !== password) {
+      throw new UnauthorizedException('Username atau password salah');
+    }
 
     return {
       message: 'Login berhasil',
-      access_token: this.jwtService.sign(payload),
-      role: user.role,
+      admin: {
+        id: admin.id,
+        username: admin.username,
+      },
     };
   }
 }
