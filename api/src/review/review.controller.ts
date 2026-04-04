7a7602118e3d52a3f055bc68068fbd59082cd98d
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:29:20 +0700

    Update review.controller.ts

--- api/src/review/review.controller.ts
+++ api/src/review/review.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
-import { UpdateReviewDto } from './dto/update-review.dto';
 
+@ApiTags('review')
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewService.create(createReviewDto);
+  create(@Body() dto: CreateReviewDto) {
+    return this.reviewService.create(dto);
   }
 
   @Get()
@@ -17,18 +18,8 @@ export class ReviewController {
     return this.reviewService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewService.update(+id, updateReviewDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.reviewService.remove(id);
   }
 }
