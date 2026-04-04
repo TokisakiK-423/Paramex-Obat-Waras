f0422d9a6d3db3fcc58aa459a69a8e07c7ceec27
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:16:19 +0700

    Update create-wisata.dto.ts

--- api/src/wisata/dto/create-wisata.dto.ts
+++ api/src/wisata/dto/create-wisata.dto.ts
@@ -1,6 +1,28 @@
+import { IsOptional, IsString, IsNumber } from 'class-validator';
+
 export class CreateWisataDto {
+  @IsString()
   nama: string;
+
+  @IsString()
   deskripsi: string;
-  harga: number;
+
+  @IsString()
   lokasi: string;
+
+  @IsOptional()
+  @IsNumber()
+  latitude?: number;
+
+  @IsOptional()
+  @IsNumber()
+  longitude?: number;
+
+  @IsOptional()
+  @IsString()
+  jamBuka?: string;
+
+  @IsOptional()
+  @IsNumber()
+  hargaTiket?: number;
 }
