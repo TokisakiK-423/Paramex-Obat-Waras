4aa1b2440467e42f989ee2c38b7885db88a10deb
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:51:02 +0700

    Update app.controller.spec.ts

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -9,14 +9,4 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
-  });
-});
+    }).c
