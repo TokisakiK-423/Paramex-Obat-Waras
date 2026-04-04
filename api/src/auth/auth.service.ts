0652a21abe9d99be3bcb45c4d83d4c762b2a660b
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:41:33 +0700

    Update auth.service.ts

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(username: string, password: string) {
     if (!username || !password) {
@@ -18,8 +22,14 @@ export class AuthService {
       throw new UnauthorizedException('Username atau password salah');
     }
 
+    const token = await this.jwtService.signAsync({
+      sub: admin.id,
+      username: admin.username,
+    });
+
     return {
       message: 'Login berhasil',
+      access_token: token,
       admin: {
         id: admin.id,
         username: admin.username,
