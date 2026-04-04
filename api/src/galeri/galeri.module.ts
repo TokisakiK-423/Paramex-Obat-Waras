2098e08d76c8fd9423dc5b44e5207477e6104a69
TokisakiK-423 <univerword@gmail.com>
2026-04-04 13:45:55 +0700

    Delete api/src directory

--- api/src/galeri/galeri.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GaleriController } from './galeri.controller';
-import { GaleriService } from './galeri.service';
-
-@Module({
-  controllers: [GaleriController],
-  providers: [GaleriService],
-})
-export class GaleriModule {}
