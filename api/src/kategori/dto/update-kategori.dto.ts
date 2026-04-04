dd8c6e6b8070deaf6426838d26bb56e5793edc6c
TokisakiK-423 <univerword@gmail.com>
2026-04-04 13:52:15 +0700

    Delete api/src/kategori/dto/update-kategori.dto.ts

--- api/src/kategori/dto/update-kategori.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateKategoriDto } from './create-kategori.dto';
-
-export class UpdateKategoriDto extends PartialType(CreateKategoriDto) {}
